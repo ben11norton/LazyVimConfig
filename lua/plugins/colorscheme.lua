5afaff2fd7203773ae2f5442c1aec04b1863f3be
Ben Norton <155587154+ben11norton@users.noreply.github.com>
2025-06-08 20:21:28 +0200

    initial neovim config commit

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,64 @@
+-- poimandres color theme:
+-- return{
+--
+--   { "olivercederborg/poimandres.nvim" },
+--
+--   -- Configure LazyVim to load gruvbox
+--   {
+--     "LazyVim/LazyVim",
+--     opts = {
+--       colorscheme = "poimandres",
+--     },
+--   },
+-- }
+
+-- rose pine color theme
+-- return{
+--
+--   { "rose-pine/neovim" },
+--
+--   {
+--     "LazyVim/LazyVim",
+--     opts = {
+--       colorscheme = "rose-pine",
+--     },
+--   },
+-- }
+
+return {
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    opts = {
+      variant = "main",
+      dark_variant = "main",
+      highlight_groups = {
+        Normal = { bg = "#000000" },
+        NormalFloat = { bg = "#000000" },
+      },
+      styles = {
+        italic = false
+      },
+      before_highlight = function (group, highlight, palette)
+        -- change color pallete:
+        if highlight.fg == palette.iris then
+            highlight.fg = palette.rose
+        elseif highlight.fg == palette.foam then
+            highlight.fg = palette.text
+        end
+      end
+    },
+    config = function(_, opts)
+      require("rose-pine").setup(opts)
+      vim.cmd.colorscheme("rose-pine")
+    end,
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "rose-pine",
+    },
+  },
+}
+
