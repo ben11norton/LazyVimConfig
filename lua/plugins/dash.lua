378ecc9905b236e1c79cca9c4e06f8d0d75f5405
Ben Norton <155587154+ben11norton@users.noreply.github.com>
2025-06-10 22:01:26 +0200

    Figured out how to change the dashboard header

---
+++ lua/plugins/dash.lua
@@ -0,0 +1,17 @@
+
+return {
+  "folke/snacks.nvim",
+  priority = 1000,
+  lazy = false,
+  opts = {
+    dashboard = {
+      enabled = true,
+      preset = {
+        header = [[
+          welcome to neovim
+        ]]
+      }
+    }
+  }
+}
+
