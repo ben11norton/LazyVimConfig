96235287faaa02209614a539ccd03df89dd0a13b
Ben Norton <155587154+ben11norton@users.noreply.github.com>
2025-06-12 19:43:01 +0200

    Finally figured out how the dashboard works: next commit needs to not be week distraction from personal project lol

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,3 +1,42 @@
+return {
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    opts = {
+      variant = "main",
+      dark_variant = "main",
+      highlight_groups = {
+        Normal = { bg = "#000000" },
+        NormalFloat = { bg = "#000000" },
+      },
+      styles = {
+        italic = false
+      },
+      before_highlight = function (group, highlight, palette)
+        -- change color pallete:
+        if highlight.fg == palette.iris then
+            highlight.fg = palette.rose
+        elseif highlight.fg == palette.foam then
+            highlight.fg = palette.text
+        elseif highlight.fg == palette.pine then
+            highlight.fg = palette.love
+        end
+      end
+    },
+    config = function(_, opts)
+      require("rose-pine").setup(opts)
+      vim.cmd.colorscheme("rose-pine")
+    end,
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "rose-pine",
+    },
+  },
+}
+
 -- poimandres color theme:
 -- return{
 --
@@ -24,41 +63,3 @@
 --     },
 --   },
 -- }
-
-return {
-  {
-    "rose-pine/neovim",
-    name = "rose-pine",
-    opts = {
-      variant = "main",
-      dark_variant = "main",
-      highlight_groups = {
-        Normal = { bg = "#000000" },
-        NormalFloat = { bg = "#000000" },
-      },
-      styles = {
-        italic = false
-      },
-      before_highlight = function (group, highlight, palette)
-        -- change color pallete:
-        if highlight.fg == palette.iris then
-            highlight.fg = palette.rose
-        elseif highlight.fg == palette.foam then
-            highlight.fg = palette.text
-        end
-      end
-    },
-    config = function(_, opts)
-      require("rose-pine").setup(opts)
-      vim.cmd.colorscheme("rose-pine")
-    end,
-  },
-
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "rose-pine",
-    },
-  },
-}
-
