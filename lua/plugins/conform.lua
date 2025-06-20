e0b0b8713d9db0fdc19f7a7acba5ea81cf42b658
Ben Norton <155587154+ben11norton@users.noreply.github.com>
2025-06-20 17:13:06 +0200

    4 space indentation

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,14 @@
+
+return {
+  "stevearc/conform.nvim",
+  opts = {
+    formatters_by_ft = {
+      lua = { "stylua" },
+    },
+    formatters = {
+      stylua = {
+        command = '"C:/Users/Ben Norton/AppData/Local/nvim-data/mason/bin/stylua.CMD"',
+      },
+    },
+  },
+}
