c45b6f6bbcba253e898422ffcdb3f0a251f07826
Ben Norton <155587154+ben11norton@users.noreply.github.com>
2025-07-03 19:52:54 +0200

    Color scheme background color

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,40 +1,43 @@
 return {
-  {
-    "rose-pine/neovim",
-    name = "rose-pine",
-    opts = {
-      variant = "main",
-      dark_variant = "main",
-      highlight_groups = {
-        Normal = { bg = "#000000" },
-        NormalFloat = { bg = "#000000" },
-      },
-      styles = {
-        italic = false
-      },
-      before_highlight = function (group, highlight, palette)
-        -- change color pallete:
-        if highlight.fg == palette.iris then
-            highlight.fg = palette.rose
-        elseif highlight.fg == palette.foam then
-            highlight.fg = palette.text
-        elseif highlight.fg == palette.pine then
-            highlight.fg = palette.love
-        end
-      end
+    {
+        "rose-pine/neovim",
+        name = "rose-pine",
+        opts = {
+            variant = "main",
+            dark_variant = "main",
+            highlight_groups = {
+                Normal = { bg = "#0A0E13" },
+                NormalFloat = { bg = "#0A0E13" },
+                FloatBorder = { bg = "#0A0E13" },
+            },
+            styles = {
+                italic = false
+            },
+            before_highlight = function(group, highlight, palette)
+                -- change color pallete:
+                if highlight.fg == palette.iris then
+                    highlight.fg = palette.rose
+                elseif highlight.fg == palette.foam then
+                    highlight.fg = palette.text
+                elseif highlight.fg == palette.pine then
+                    highlight.fg = palette.love
+                elseif highlight.fg == palette.love then
+                    highlight.fg = "#e85c7a"
+                end
+            end
+        },
+        config = function(_, opts)
+            require("rose-pine").setup(opts)
+            vim.cmd.colorscheme("rose-pine")
+        end,
     },
-    config = function(_, opts)
-      require("rose-pine").setup(opts)
-      vim.cmd.colorscheme("rose-pine")
-    end,
-  },
 
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "rose-pine",
+    {
+        "LazyVim/LazyVim",
+        opts = {
+            colorscheme = "rose-pine",
+        },
     },
-  },
 }
 
 -- poimandres color theme:
