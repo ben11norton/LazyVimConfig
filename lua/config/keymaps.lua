106401b0d2e73aed37054b5270c0c035253c25c0
Ben Norton <155587154+ben11norton@users.noreply.github.com>
2025-07-17 20:22:29 +0200

    Dap ui

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,24 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+
+-- DAP keybindings
+local dap = require("dap")
+
+vim.keymap.set("n", "<F5>", function() dap.continue() end, { desc = "DAP: Continue" })
+vim.keymap.set("n", "<F10>", function() dap.step_over() end, { desc = "DAP: Step Over" })
+vim.keymap.set("n", "<F11>", function() dap.step_into() end, { desc = "DAP: Step Into" })
+vim.keymap.set("n", "<F12>", function() dap.step_out() end, { desc = "DAP: Step Out" })
+
+vim.keymap.set("n", "<Leader>b", function() dap.toggle_breakpoint() end, { desc = "DAP: Toggle Breakpoint" })
+vim.keymap.set("n", "<Leader>B", function()
+    dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
+end, { desc = "DAP: Set Conditional Breakpoint" })
+
+vim.keymap.set("n", "<Leader>lp", function()
+    dap.set_breakpoint(nil, nil, vim.fn.input("Log point message: "))
+end, { desc = "DAP: Set Log Point" })
+
+vim.keymap.set("n", "<Leader>dr", function() dap.repl.open() end, { desc = "DAP: Open REPL" })
+vim.keymap.set("n", "<Leader>dl", function() dap.run_last() end, { desc = "DAP: Run Last" })
--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -5,31 +5,54 @@ return {
         config = function()
             local dap = require("dap")
 
-            -- only register netcoredbg if it's not already there
             if not dap.adapters.netcoredbg then
                 dap.adapters.netcoredbg = {
-                    type    = "executable",
+                    type = "executable",
                     command = "C:\\Dev\\tools\\netcoredbg\\netcoredbg.exe",
-                    args    = { "--interpreter=vscode" },
+                    args = { "--interpreter=vscode" },
                     options = { detached = false },
                 }
             end
 
-            -- add C#/F#/VB launch configs
             for _, lang in ipairs({ "cs", "fsharp", "vb" }) do
                 dap.configurations[lang] = dap.configurations[lang] or {
                     {
-                        type    = "netcoredbg",
-                        name    = "Launch file",
+                        type = "netcoredbg",
+                        name = "Launch file",
                         request = "launch",
                         program = function()
-                            return vim.fn.input("Path to dll: ",
-                                vim.fn.getcwd() .. "\\",
-                                "file")
+                            return vim.fn.input("Path to dll: ", vim.fn.getcwd() .. "\\", "file")
                         end,
-                        cwd     = "${workspaceFolder}",
+                        cwd = "${workspaceFolder}",
                     },
                 }
+            end
+        end,
+    },
+
+    -- Required by nvim-dap-ui
+    {
+        "nvim-neotest/nvim-nio",
+    },
+
+    -- DAP UI
+    {
+        "rcarriga/nvim-dap-ui",
+        dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
+        config = function()
+            local dap = require("dap")
+            local dapui = require("dapui")
+
+            dapui.setup()
+
+            dap.listeners.after.event_initialized["dapui_config"] = function()
+                dapui.open()
+            end
+            dap.listeners.before.event_terminated["dapui_config"] = function()
+                dapui.close()
+            end
+            dap.listeners.before.event_exited["dapui_config"] = function()
+                dapui.close()
             end
         end,
     },
